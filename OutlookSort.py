09ad0bd3a33cff1463a17c2d16e9de0b9d08945f
tsmith4014 <chjthomps@gmail.com>
2024-05-02 14:17:02 -0400

    added requirements.txt

--- OutlookSort.py
+++ OutlookSort.py
@@ -47,7 +47,7 @@ def get_all_messages(folder, folder_path=''):
 
     return messages
 
-file_path = os.path.expanduser("/Users/chadthompsonsmith/Desktop/Projects/amy_projects/OutlookEmailSort/Data_Sort/Inbox/backup.pst")
+file_path = os.path.expanduser("/Users/chadthompsonsmith/Desktop/Projects/amy_projects/OutlookEmailSort/Data_Sort/Inbox/oldbackup.pst")
 
 # Open the PST file
 pst_file = pypff.file()
